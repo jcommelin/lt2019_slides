672d48b17c22bbe39343f26459e305078ed6c7d8
Johan M. Commelin <johan@commelin.net>
2019-01-05 13:07:09 +0100

    WIP

--- category.lean
+++ category.lean
@@ -1,12 +1,11 @@
 class category (obj : Type u) : Type (max u (v+1)) :=
 (hom      : obj → obj → Type v)
-(id       : Π X : obj, hom X X)
-(comp     : Π {X Y Z : obj}, hom X Y → hom Y Z → hom X Z)
-(id_comp' : ∀ {X Y : obj} (f : hom X Y), comp (id X) f = f . obviously)
-(comp_id' : ∀ {X Y : obj} (f : hom X Y), comp f (id Y) = f . obviously)
-(assoc'   : ∀ {W X Y Z : obj} (f : hom W X) (g : hom X Y) (h : hom Y Z),
-            comp (comp f g) h = comp f (comp g h) . obviously)
-
-notation `𝟙` := category.id -- type as \b1
-infixr ` ≫ `:80 := category.comp -- type as \gg
-infixr ` ⟶ `:10 := category.hom -- type as \h
+(infixr ` ⟶ `:10 := hom)
+(id       : Π X : obj, X ⟶ X)
+(notation `𝟙` := id)
+(comp     : Π {X Y Z : obj}, (X ⟶ Y) → (Y ⟶ Z) → (X ⟶ Z))
+(infixr ` ≫ `:80 := comp)
+(id_comp' : ∀ {X Y : obj} (f : X ⟶ Y), 𝟙 X ≫ f = f . obviously)
+(comp_id' : ∀ {X Y : obj} (f : X ⟶ Y), f ≫ 𝟙 Y = f . obviously)
+(assoc'   : ∀ {W X Y Z : obj} (f : W ⟶ X) (g : X ⟶ Y) (h : Y ⟶ Z),
+  (f ≫ g) ≫ h = f ≫ (g ≫ h) . obviously)
